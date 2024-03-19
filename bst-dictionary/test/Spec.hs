74c53a230808288e4c7bf30142504ac2e2dab499
AlxTray <alex.ashby02@icloud.com>
2024-03-19 23:33:39 +0000

    Iteration 3, Test Failure: test to check value more than parent is inserted on right

--- bst-dictionary/test/Spec.hs
+++ bst-dictionary/test/Spec.hs
@@ -12,11 +12,18 @@ testInsertOnLeft = TestCase $ do
     let expectedTree = (Node 5 "Dave" (Node 3 "Jeff" Empty Empty) Empty)
     assertEqual "Test to see insert with a key less than parent is inserted on left" tree expectedTree
 
+testInsertOnRight :: Test
+testInsertOnRight = TestCase $ do
+    let tree = insert 10 "Jeff" (insert 5 "Dave" emptyTree)
+    let expectedTree = (Node 5 "Dave" Empty (Node 10 "Jeff" Empty Empty))
+    assertEqual "Test to see insert with a key more than parent is inserted on right" tree expectedTree
+
 
 allTests :: Test
 allTests = TestList [
     testEmptyInsert,
-    testInsertOnLeft
+    testInsertOnLeft,
+    testInsertOnRight
  ]
 
 
