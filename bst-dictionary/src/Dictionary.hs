7c148eb2069ad65c3eac3c52fcc7e8b097f16bee
AlxTray <alex.ashby02@icloud.com>
2024-04-26 00:33:55 +0100

    feat: added property-based tests

--- bst-dictionary/src/Dictionary.hs
+++ bst-dictionary/src/Dictionary.hs
@@ -1,24 +1,27 @@
 module Dictionary where
 
+import Prelude hiding (lookup)
 import BST
 
+
 data Dictionary key item = Dictionary (BST key item)
+    deriving (Eq, Show)
 
 
 newDict :: Dictionary key item
-newDict = emptyTree
+newDict = Dictionary emptyTree
 
-insertDict :: key -> item -> Dictionary key item -> Dictionary key item
+insertDict :: Ord key => key -> item -> Dictionary key item -> Dictionary key item
 insertDict key item (Dictionary dict) = Dictionary (insert key item dict)
 
-lookupDict :: key -> Dictionary key item -> Maybe item
+lookupDict :: Ord key => key -> Dictionary key item -> Maybe item
 lookupDict key (Dictionary dict) = lookup key dict
 
-listDict :: Dictionary key item -> [(key, item)]
+listDict :: Ord key => Dictionary key item -> [(key, item)]
 listDict (Dictionary dict) = list dict
 
-removeDict :: key -> Dictionary key item -> Dictionary key item
-removeDict removeKey (Dictionary dict) = remove removeKey dict
+removeDict :: (Ord key, Eq item) => key -> Dictionary key item -> Dictionary key item
+removeDict removeKey (Dictionary dict) = Dictionary (remove removeKey dict)
 
-removeAllDict :: (key -> Bool) -> Dictionary key item -> Dictionary key item
-removeAllDict predicate (Dictionary dict) = removeAll predicate dict
+removeAllDict :: (Ord key, Eq item) => (key -> Bool) -> Dictionary key item -> Dictionary key item
+removeAllDict predicate (Dictionary dict) = Dictionary (removeAll predicate dict)
--- bst-dictionary/test/DictionarySpec.hs
+++ bst-dictionary/test/DictionarySpec.hs
@@ -3,12 +3,68 @@ module DictionarySpec where
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
+import Data.Maybe
+import qualified Data.Set as Set
 
 import Dictionary
+import BST
 
-propertyNewDictionaryEmpty Dictionary key item -> Property
-propertyNewDictionaryEmpty = 
+
+instance (Arbitrary key, Arbitrary item, Ord key) => Arbitrary (BST key item) where
+    -- This is done so an empty set for the keys to be stored can be passed
+    arbitrary = sized (\numberOfNodes -> generateSubTree numberOfNodes Set.empty)
+
+generateSubTree :: (Arbitrary key, Arbitrary item, Ord key) => Int -> Set.Set key -> Gen (BST key item)
+generateSubTree 0 _ = return Empty
+generateSubTree numberOfNodes keysInTree = do
+    -- Use of set to only allow unique keys idea adapted from question at
+    -- https://stackoverflow.com/questions/35528777/quickcheck-produce-arbitrary-elements-of-an-arbitrary-set
+    key <- arbitrary `suchThat` (\randomKey -> not (Set.member randomKey keysInTree))
+    item <- arbitrary
+    leftSize <- choose (0, numberOfNodes - 1)
+    rightSize <- choose (0, numberOfNodes - 1)
+
+    let keysInTreeWithNew = Set.insert key keysInTree
+    left <- generateSubTree leftSize keysInTreeWithNew
+    right <- generateSubTree rightSize keysInTreeWithNew
+
+    return $ Node key item left right
+
+
+instance (Arbitrary key, Arbitrary item, Ord key) => Arbitrary (Dictionary key item) where
+    arbitrary = do
+        tree <- arbitrary
+        return $ Dictionary tree
+
+
+prop_insertThenLookup :: Int -> Int -> Dictionary Int Int -> Property
+prop_insertThenLookup key item dict =
+    property (lookupDict key (insertDict key item dict) == Just item)
+
+prop_removeAllThenList :: Dictionary Int Int -> Property
+prop_removeAllThenList dict =
+    property (null (listDict (removeAllDict (const True) dict)))
+
+prop_removeThenLookup :: Dictionary Int Int -> Property
+prop_removeThenLookup dict =
+    property (lookupDict removeKey (removeDict removeKey dict) === Nothing)
+    where
+        -- Gets a particular value from the tree
+        -- Otherwise a random key may not be in the random tree
+        (removeKey, _) = head (listDict dict)
+
+prop_emptyThenList :: Property
+prop_emptyThenList =
+    let emptyTree :: Dictionary Int Int
+        emptyTree = newDict
+    in
+        property (null (listDict emptyTree))
+
 
 allDictionaryTests :: TestTree
-allDictionaryTests = testGroup "All tests for the Dictionary module"
-    []
+allDictionaryTests = testGroup "All tests for the Dictionary module (Property-based)"
+    [ testProperty "A lookup after an insertion should return the inserted item" prop_insertThenLookup,
+      testProperty "Listing after removing all nodes in the tree should be empty" prop_removeAllThenList,
+      testProperty "Removing then attempting to lookup the removed value should return Nothing" prop_removeThenLookup,
+      testProperty "Listing a new dictionary should return an empty list" prop_emptyThenList
+    ]
